5e693e2ddd169ef1ed14eff45dc8d88c4c626d21
AbdElRahmanYehya <a.yehya.aboelella@gmail.com>
2021-10-06 19:07:43 +0200

    Switch between Answered and Unanswered

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,14 +6,55 @@ import UnansweredQuestions from './UnansweredQuestions'
 
 
 class HomePage extends Component {
+	state = {
+		answered: false,
+	}
+
+	showAnswered = (e) => {
+		e.preventDefault()
+
+		const {dispatch, id} = this.props
+		console.log('id is :', id)
+
+		this.setState(() => ({
+			answered: true,
+		}))
+	}
+
+	showunAnswered = (e) => {
+		e.preventDefault()
+
+		const {dispatch, id} = this.props
+		console.log('id is :', id)
+
+		this.setState(() => ({
+			answered: false,
+		}))
+	}
+
 	render() {
+		const { answered } = this.state
 		console.log(this.props)
 		return (
 			<div>
 				<h3 className='center'>All questions</h3>
-				<ul className='dashboard-list'>
-					<UnansweredQuestions />
-				</ul>
+				<button className='btn' onClick={this.showAnswered}>
+					Answered Questions
+				</button>
+				<button className='btn' onClick={this.showunAnswered}>
+					Unanswered Questions
+				</button>
+				{answered === true
+								? <div>
+									<ul className='dashboard-list'>
+										<AnsweredQuestions />
+									</ul>
+								   </div>
+								: <div>
+									<ul className='dashboard-list'>
+										<UnansweredQuestions />
+									</ul>
+								   </div>}
 			</div>
 		)
 	}
