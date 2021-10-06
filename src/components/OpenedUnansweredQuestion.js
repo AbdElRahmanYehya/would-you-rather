1dd1a662c43e4f2f32db1b101c89ec0269020dea
AbdElRahmanYehya <a.yehya.aboelella@gmail.com>
2021-10-06 18:46:51 +0200

    Basic question UI

--- src/components/OpenedUnansweredQuestion.js
+++ src/components/OpenedUnansweredQuestion.js
@@ -5,11 +5,30 @@ import ClosedUnansweredQuestion from './ClosedUnansweredQuestion'
 
 class OpenedUnansweredQuestion extends Component {
 	render() {
+	const { question } = this.props
+	// mfrod nbdl question el t7t b el format da
+	// formatQuestion(question.optionOne, question.optionTwo, question.author)
+	const {
+		author, name, avatarURL, timestamp, optionOne, optionTwo
+	} = question
 	console.log(this.props)
 	const {id} = this.props
 		return (
 			<div className='tweet'>
-				<ClosedUnansweredQuestion id={id}/>
+				<form onSubmit={this.handleSubmit}>
+					<img
+						src={avatarURL}
+						alt={`Avatar of ${author}`}
+						className='avatar'
+					/>
+					<input type="radio" id="firstOption" name="chooseAnswer" value="firstOption"/>
+					<label>{optionOne.text}</label>
+					<input type="radio" id="secondOption" name="chooseAnswer" value="secondOption"/>
+					<label>{optionTwo.text}</label>
+					<button className='btn' type='submit'>
+						See poll
+					</button>
+				</form>
 			</div>
 		)
 	}
@@ -17,8 +36,10 @@ class OpenedUnansweredQuestion extends Component {
 
 function mapStateToProps ({authedUser, questions, users}, props) {
 	const {id} = props.match.params
-
+	const question = questions[id]
 	return {
+		authedUser,
+		question: question,
 		id,
 	}
 }
